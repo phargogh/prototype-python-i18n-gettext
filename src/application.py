592914f67784ad099557f745dba65eff734bd7ba
James Douglass <jamesdouglassusa@gmail.com>
2021-02-17 12:08:27 -0800

    Starting to flesh out application a bit more.

--- src/application.py
+++ src/application.py
@@ -1,4 +1,22 @@
+import argparse
 import gettext
 
-def main():
+
+
+# TODO: set language
+# TODO: check that provided language is known.
+#       warn and fall back to english if not.
+
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        description='i18n hello world')
+    parser.add_argument('lang')
+
+    parsed_args = parser.parse_args(args)
+    print(parsed_args)
+
     print("Hello, world!")
+
+
+if __name__ == '__main__':
+    main()
