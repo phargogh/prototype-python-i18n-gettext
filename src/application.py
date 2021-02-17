18e2f1f235213f320d430418f315d50930770ec6
James Douglass <jamesdouglassusa@gmail.com>
2021-02-17 14:04:58 -0800

    Cleaning up a few TODOs.

--- src/application.py
+++ src/application.py
@@ -4,10 +4,6 @@ import os
 
 
 
-# TODO: set language
-# TODO: use set language.
-# TODO: check that provided language is known.
-#       warn and fall back to english if not.
 # TODO: highlight text that's changed since the last translation update.
 #       Goal: only send strings that have changed to the translator.
 
@@ -17,11 +13,12 @@ def main(args=None):
     parser.add_argument('lang')
 
     parsed_args = parser.parse_args(args)
+    domain = 'messages'
     localedir = os.path.join(os.path.dirname(__file__), 'i18n')
     print(parsed_args)
 
     gettext.translation(
-        'messages',
+        domain,
         localedir=localedir,
         fallback=True,  # Use english if lang not found
         languages=[parsed_args.lang]).install()
