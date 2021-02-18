b87aa1892e00d87c7dfeae745626bb2c04c9675e
James Douglass <jamesdouglassusa@gmail.com>
2021-02-17 18:12:24 -0800

    Loading directly from a .mo file.

--- src/application.py
+++ src/application.py
@@ -17,13 +17,20 @@ def main(args=None):
     localedir = os.path.join(os.path.dirname(__file__), 'i18n')
     print(parsed_args)
 
-    gettext.translation(
-        domain,
-        localedir=localedir,
-        fallback=True,  # Use english if lang not found
-        languages=[parsed_args.lang]).install()
+    # If we're provided with a .mo file, just use that.
+    if os.path.exists(parsed_args.lang):
+        print(f'Using .mo file at {parsed_args.lang}')
+        with open(parsed_args.lang, 'rb') as mo_file:
+            gettext.GNUTranslations(mo_file).install()
+    else:
+        print(f"Loading translation for {parsed_args.lang} from {localedir}")
+        gettext.translation(
+            domain,
+            localedir=localedir,
+            fallback=True,  # Use english if lang not found
+            languages=[parsed_args.lang]).install()
 
-    # NOTE: This is the standard greeting.
+    # I18N-NOTE: This is the standard greeting.
     print(_("Hello, world!"))
 
 
